58fa86f35c4f0cfed70d5302c27ee23177328c59
Michael Nizzari <michael.nizzari@gmail.com>
2017-06-27 14:28:33 -0400

    002 - Variable Printer

---
+++ 002-variable-printer/readme.md
@@ -0,0 +1,3 @@
+# Variable Printer
+
+Stores string and integer values into variables, then prints them.
---
+++ 002-variable-printer/var_printer.py
@@ -0,0 +1,6 @@
+message = "This is a string value that has been stored in a variable."
+print(message)
+
+print("The following is an integer value that was stored in a variable:")
+num = 5
+print(num)
