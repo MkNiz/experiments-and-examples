0d07423212f191cae6476ecd9c5a42e4d6ec76cc
Michael Nizzari <michael.nizzari@gmail.com>
2017-06-28 13:22:26 -0400

    017 - If Statements

---
+++ 017-if-statements/if.py
@@ -0,0 +1,91 @@
+good = False
+
+print("For value of good:")
+print(good)
+print("")
+
+print("If good is True, print 'you're good' (no else):")
+if good == True:
+    print("you're good")
+print("---")
+
+good = True
+
+print("For value of good:")
+print(good)
+print("")
+
+print("If good is True, print 'you're good' (no else):")
+if good == True:
+    print("you're good")
+print("---")
+
+good = False
+
+print("For value of good:")
+print(good)
+print("")
+
+print("If good is True, print 'you're good'. If not, print 'you're bad':")
+if good == True:
+    print("you're good")
+else:
+    print("you're bad")
+print("---")
+
+good = True
+
+print("For value of good:")
+print(good)
+print("")
+
+print("If good is True, print 'you're good'. If not, print 'you're bad':")
+if good == True:
+    print("you're good")
+else:
+    print("you're bad")
+print("---")
+
+goodness = "marginal"
+
+print("For value of goodness:")
+print(goodness)
+print("")
+
+print("Print the appropriate response:")
+if goodness == "bad":
+    print("you're bad")
+elif goodness == "marginal":
+    print("you're just ok")
+else:
+    print("you're good")
+print("---")
+
+goodness = "bad"
+
+print("For value of goodness:")
+print(goodness)
+print("")
+
+print("Print the appropriate response:")
+if goodness == "bad":
+    print("you're bad")
+elif goodness == "marginal":
+    print("you're just ok")
+else:
+    print("you're good")
+print("---")
+
+goodness = "excellent"
+
+print("For value of goodness:")
+print(goodness)
+print("")
+
+print("Print the appropriate response:")
+if goodness == "bad":
+    print("you're bad")
+elif goodness == "marginal":
+    print("you're just ok")
+else:
+    print("you're good")
---
+++ 017-if-statements/readme.md
@@ -0,0 +1,3 @@
+# If Statements
+
+Demonstrates simple if, else, and elif statements.
