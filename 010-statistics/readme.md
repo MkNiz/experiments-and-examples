651f938738d550495e2949b05863101fd9dfe2a6
Michael Nizzari <michael.nizzari@gmail.com>
2017-06-28 11:50:04 -0400

    010 - Statistics

---
+++ 010-statistics/readme.md
@@ -0,0 +1,4 @@
+# Statistics
+
+Uses some of the statistical functions available in Python to print some basic statistics
+for a given list.
---
+++ 010-statistics/statistics.py
@@ -0,0 +1,20 @@
+my_list_4 = [4, 16, 5, 12, 69]
+
+print("Assuming the following list:")
+print(my_list_4)
+print("")
+
+print("The smallest number, using the min function, is:")
+print(min(my_list_4))
+print("")
+
+print("The largest number, using the max function, is:")
+print(max(my_list_4))
+print("")
+
+print("The sum of all values, using the sum function, is:")
+print(sum(my_list_4))
+print("")
+
+print("The average of all values, performed manually with sum and len, is:")
+print(sum(my_list_4)/len(my_list_4))
