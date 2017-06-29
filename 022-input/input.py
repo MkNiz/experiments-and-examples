e640eecccf3f74b54ee3c7cf0cf0ae75dd144c68
Michael Nizzari <michael.nizzari@gmail.com>
2017-06-29 11:11:53 -0400

    022 - Input

---
+++ 022-input/input.py
@@ -0,0 +1,2 @@
+msg = input("This will repeat whatever is typed back, but with ', heck yea!' concatenated to the end.\n")
+print(msg + ", heck yea!")
---
+++ 022-input/readme.md
@@ -0,0 +1,4 @@
+# Input
+
+Utilizes the input() function to repeat the value that is input with ', heck yea!'
+concatenated to the end.
